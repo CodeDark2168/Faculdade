6e4e56f42d705ee22abffe1ba15202a4cc0d4313
Otávio Sudano <116005183+CodeDark2168@users.noreply.github.com>
2024-05-02 14:05:25 -0300

    Add files via upload

--- Algoritmo/Aula [10]/exercicio05.py
+++ Algoritmo/Aula [10]/exercicio05.py
@@ -4,10 +4,8 @@ quantidade = int(input("Informe a quantidade de vendas: "))
 for i in range(quantidade):
     total = float(input("Informe o total da venda: "))
     soma += total
-
-if total > 500:
-    imposto = soma * 0.15
-else:
-    imposto = soma * 0.11
-
-print("O imposto será de:",imposto)
+    if total > 500:
+        imposto = soma * 0.15
+    else:
+        imposto = soma * 0.11
+    print("O imposto será de:",imposto)
