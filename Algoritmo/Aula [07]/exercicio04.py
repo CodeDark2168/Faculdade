a339f3c272cb541876a8adda7118e54545634f98
Otávio Sudano <116005183+CodeDark2168@users.noreply.github.com>
2024-05-23 13:29:41 -0300

    Delete Algoritmo/Aula [07] directory

--- Algoritmo/Aula [07]/exercicio04.py
+++
@@ -1,7 +0,0 @@
-print("==============================")
-ano = int(input("Informe o ano: "))
-if ano % 4 == 0:
-    print("O ano é bissexto")
-else:
-    print("O ano não é bissexto")
-print("==============================")
