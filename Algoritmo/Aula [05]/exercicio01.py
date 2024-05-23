8ccde81c7ff1b9d8df8b8bfa3c6376ae9b65734d
Otávio Sudano <116005183+CodeDark2168@users.noreply.github.com>
2024-05-23 13:28:59 -0300

    Delete Algoritmo/Aula [05] directory

--- Algoritmo/Aula [05]/exercicio01.py
+++
@@ -1,8 +0,0 @@
-print("==============================")
-idade = int(input("Informe sua idade: "))
-
-if idade > 17:
-    print("Maior de idade")
-else:
-    print("Menor de idade")
-print("==============================")
