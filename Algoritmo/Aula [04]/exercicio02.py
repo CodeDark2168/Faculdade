e2f61427881988bd3d7008bc5ce20008048e344e
Otávio Sudano <116005183+CodeDark2168@users.noreply.github.com>
2024-05-23 13:28:47 -0300

    Delete Algoritmo/Aula [04] directory

--- Algoritmo/Aula [04]/exercicio02.py
+++
@@ -1,8 +0,0 @@
-import math
-
-print("==============================")
-valorA = int(input("Digite o valor de A: "))
-valorB = int(input("Digite o valor de B: "))
-calculo = math.sqrt(math.sin(valorA) + math.sin(valorB))
-print("O resultado do calculo √senA + senB é " + str(round(calculo, 2)))
-print("==============================")
