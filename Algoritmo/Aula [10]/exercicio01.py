4b59c73fcebe583c2e400a64060151440fd45891
Otávio Sudano <116005183+CodeDark2168@users.noreply.github.com>
2024-05-23 13:30:05 -0300

    Delete Algoritmo/Aula [10] directory

--- Algoritmo/Aula [10]/exercicio01.py
+++
@@ -1,4 +0,0 @@
-valor = int(input("Informe um valor: "))
-
-for i in range(valor,0,-1):
-    print(i)
--- Algoritmo/Aula [10]/exercicio02.py
+++
@@ -1,4 +0,0 @@
-quantidade = int(input("Informe a quantidade de notas: "))
-
-for i in range(quantidade):
-    nota = float(input("Informe a nota: "))
