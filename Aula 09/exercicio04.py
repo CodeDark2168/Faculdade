00a76679f64edd2f2789a31db7d837d5e9b0bb51
Otávio Sudano <116005183+CodeDark2168@users.noreply.github.com>
2024-05-23 14:04:44 -0300

    Delete Aula 09 directory

--- Aula 09/exercicio04.py
+++
@@ -1,5 +0,0 @@
-valor = int(input("Informe o valor que deseja a tabuada: "))
-
-for i in range(1, 11): 
-    resultado = valor * i 
-    print(valor,"x",i,"=",resultado)
--- Aula 09/exercicio07.py
+++
@@ -1,5 +0,0 @@
-nome = input("Informe o nome: ")
-valor = int(input("Informe número de vezes que vai repetir: "))
-
-for i in range(valor):
-    print(nome)
