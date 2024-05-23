2f58eebf837b18b53ef650f7b994b19915a09475
Otávio Sudano <116005183+CodeDark2168@users.noreply.github.com>
2024-05-23 13:28:35 -0300

    Delete Algoritmo/Aula [03] directory

--- Algoritmo/Aula [03]/exercicio02.py
+++
@@ -1,7 +0,0 @@
-print("==============================")
-nota1 = float(input("Digite a primeira nota: "))
-nota2 = float(input("Digite a segunda nota: "))
-media = (nota1 * 0.5) + (nota2 * 0.5)
-print("==============================")
-print("Sua média é: " + str(media))
-print("==============================")
--- Algoritmo/Aula [03]/exercicio09.py
+++
@@ -1,8 +0,0 @@
-print("==============================")
-salario = float(input("Digite o valor do seu salário: "))
-imposto = salario * 0.15
-inss = salario * 0.17
-total = salario - imposto - inss
-print("==============================")
-print("Seu salário com o desconto do imposto e INSS ficou R$" + str(round(total , 2)))
-print("==============================")
